6dd07798edd5d90de28800326f927cd728f668ac
Gustaf Lindqvist <gustaf.lindqvist@gmail.com>
2023-02-28 21:28:23 +0100

    Fix the backgrounds and vertical platform script

---
+++ Assets/Scripts/VerticalPlatform.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalPlatform : MonoBehaviour
+{
+    //[SerializeField] float
+    private PlatformEffector2D effector;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
